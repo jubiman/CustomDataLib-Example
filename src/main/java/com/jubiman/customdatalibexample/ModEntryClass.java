402863880de8710c8e9398d4f049dc825d2db66e
jubiman <mellebosboom@hotmail.com>
2023-08-17 06:59:26 +0200

    Slightly fix the example

--- src/main/java/com/jubiman/customdatalibexample/ModEntryClass.java
+++ src/main/java/com/jubiman/customdatalibexample/ModEntryClass.java
@@ -1,14 +1,16 @@
 package com.jubiman.customdatalibexample;
 
-import com.jubiman.celtest.sanity.SanityPlayersHandler;
 import com.jubiman.customdatalib.player.CustomPlayerRegistry;
+import com.jubiman.customdatalibexample.packet.PacketSyncExamplePlayer;
 import com.jubiman.customdatalibexample.player.ExamplePlayersHandler;
 import necesse.engine.modLoader.annotations.ModEntry;
+import necesse.engine.registries.PacketRegistry;
 
 @ModEntry
 public class ModEntryClass {
 	public void init() {
 		CustomPlayerRegistry.registerClass(ExamplePlayersHandler.name, SanityPlayersHandler.class);
 
+		PacketRegistry.registerPacket(PacketSyncExamplePlayer.class);
 	}
 }
